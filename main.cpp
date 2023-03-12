ef11389c9e7b4679cfe0c52a4af0a383a11e88a3
Krzysztof Adamek <kadamek@alx.pl>
2023-03-12 15:44:16 +0100

    Pierwsza wersja gry

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 int main() {
+    std::srand(std::time(nullptr));
     std::cout << "ZGADYWANKA\n";
     std::cout << "Mysle o liczbie z zakresu 1-1000. Zgadnij co to za liczba:";
-    //TODO zrobic gre
+    int wylosowana = std::rand() % 999 + 1;
+    int n;
+    std::cin >> n;
+    if (n == wylosowana)
+        std::cout << "Brawo!\n";
+    else {
+        std::cout << "Pudlo! Myslalem o " << wylosowana << '\n';
+    }
 }
