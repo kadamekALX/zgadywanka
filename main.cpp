2bbba704b28ca2088816f74f5a341f1de52e3d65
Krzysztof Adamek <kadamek@alx.pl>
2023-03-12 16:56:49 +0100

    Pytanie do skutku

--- main.cpp
+++ main.cpp
@@ -8,10 +8,8 @@ int main() {
     std::cout << "Mysle o liczbie z zakresu 1-1000. Zgadnij co to za liczba:";
     int wylosowana = std::rand() % 999 + 1;
     int n;
-    std::cin >> n;
-    if (n == wylosowana)
-        std::cout << "Brawo!\n";
-    else {
-        std::cout << "Pudlo! Myslalem o " << wylosowana << '\n';
+    while (std::cin >> n, n != wylosowana) {
+        std::cout << "Zgaduj:";
     }
+    std::cout << "Brawo!\n";
 }
